358bcc8b1c73ca0bb3653273477019f4c30f84b7
Daniel Adams <adamsdanieldamilola@gmail.com>
2026-06-07 23:27:07 +0100

    Fix Extras custom settings views not rendering by caching the dynamically created FrameworkElements in the converter

--- Converters/ExtraSettingsViewConverter.cs
+++ Converters/ExtraSettingsViewConverter.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Windows.Data;
 
 namespace Spur.Converters;
 
 public sealed class ExtraSettingsViewConverter : IValueConverter
 {
+    private static readonly ConditionalWeakTable<Spur.Extensions.IExtra, System.Windows.FrameworkElement> _cache = new();
+
     public object? Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
     {
         if (value is Spur.Extensions.IExtra extra)
         {
-            var view = extra.CreateSettingsView();
+            if (!_cache.TryGetValue(extra, out var view))
+            {
+                view = extra.CreateSettingsView();
+                if (view != null) _cache.Add(extra, view);
+            }
+
             if (parameter is string param && param == "Visibility")
             {
                 return view is null ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
